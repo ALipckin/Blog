8c7c4380f5f20e4a5a614b81c141ccb5d1a5242b
aleks lipkin <aleks.lipckin@gmail.com>
2024-01-07 13:36:33 +0500

    added services

--- app/Http/Controllers/Admin/Post/EditController.php
+++ app/Http/Controllers/Admin/Post/EditController.php
@@ -7,13 +7,13 @@ use App\Models\Category;
 use App\Models\Post;
 use App\Models\Tag;
 
-class EditController extends Controller
+class EditController extends BaseController
 {
     public function __invoke(Post $post)
     {
         $categories = Category::all();
         $tags = Tag::all();
-      
+
         return view('admin.post.edit', compact('post', 'categories', 'tags'));
     }
 }
