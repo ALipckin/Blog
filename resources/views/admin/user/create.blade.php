90c0a207917bc0b3a53b20a1dfbc9b9b4516ab06
aleks lipkin <aleks.lipckin@gmail.com>
2024-01-19 15:00:45 +0500

    Added general cosmetic changes

--- resources/views/admin/user/create.blade.php
+++ resources/views/admin/user/create.blade.php
@@ -42,13 +42,6 @@
                     <div class="text-danger">{{$message}}</div>
                     @enderror
                 </div>
-                <div class="form-group">
-                    <label>Пароль</label>
-                    <input type="text" class="form-control" name="password" placeholder="password">
-                    @error('password')
-                    <div class="text-danger">{{$message}}</div>
-                    @enderror
-                </div>
                 <div class="form-group w-50">
                     <label>Выберите пользователя</label>
                     <select name="role" class="form-control">
