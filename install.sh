d6fcdf318ac5bf66bf29a6460c51caa30e08e7d4
alipkin <aleks.lipckin@gmail.com>
2025-04-11 01:46:45 +0500

    feat: added docker & install script

---
+++ install.sh
@@ -0,0 +1,15 @@
+docker compose up -d --build
+
+sudo chown -R www-data:www-data ./
+sudo chmod -R 777 ./
+
+docker compose exec app bash -c "
+  cd /var/www &&
+  composer install &&
+  php artisan key:generate &&
+  php artisan config:cache &&
+  php artisan migrate --force &&
+  php artisan storage:link &&
+  npm install &&
+  npm run build
+"
