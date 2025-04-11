602b452252ce9345cc024b1992df59b241f04e0b
alipkin <aleks.lipckin@gmail.com>
2025-04-12 00:02:41 +0500

    fix: post header

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -2,7 +2,7 @@
 @section('content')
 <main class="blog">
     <div class="container">
-        <h1 class="edica-page-title" data-aos="fade-up">Blog</h1>
+        <h1 class="edica-page-title" data-aos="fade-up">Публикации</h1>
         <section class="featured-posts-section">
             <div class="row">
                 @foreach($posts as $post)
