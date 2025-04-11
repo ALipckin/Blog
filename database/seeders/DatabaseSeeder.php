790882fca37069ec6be42551382ff2054e91c92c
alipkin <aleks.lipckin@gmail.com>
2025-04-12 00:35:48 +0500

    fix: default user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\Post;
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,8 +17,10 @@ class DatabaseSeeder extends Seeder
          User::factory(5)->create();
 
          User::factory()->create([
-             'name' => 'Test User',
-             'email' => 'test@example.com',
+             'name' => 'admin',
+             'email' => 'admin@admin.admin',
+             'password' => Hash::make('admin'),
+             'role' => User::ROLE_ADMIN,
          ]);
 
          Post::factory(20)->create();
--- readme.md
+++ readme.md
@@ -1,6 +1,17 @@
 # Blog
 ___
 ## Installation
-run install.sh
+run: `./install.sh`
+
+
+main page: http://localhost:8876/
+admin panel: http://localhost:8876/admin
+default admin user:
+login: admin@admin.admin
+password: admin
 
 ![img.png](img.png)
+
+Admin panel:
+
+![img_1.png](img_1.png)
